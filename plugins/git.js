9e83597ad3cad020a0d1af1aced85f3181fc7613
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-09-18 11:57:33 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -18,14 +18,8 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
-*owner number wa.me/919895828468*
-
-*bot setting video : https://youtu.be/RlSG4ZgPls0*
-
-*github link : https://github.com/cyberchekuthan/Amalser_v1*
-
-*audio commads : https://github.com/cyberchekuthan/Amalser_v1/tree/master/uploads*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `
+*Seriously Dude?*
 `}) 
 
 }));
